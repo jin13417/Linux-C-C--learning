79bcb8c12dee8f8c5835ca8e09b659eaba58faed
jinshaohui <jinshaohui789@163.com>
2018-09-07 21:16:18 -0400

    new

--- Algorithm/sort/sort.c
+++ Algorithm/sort/sort.c
@@ -7,8 +7,75 @@
  ************************************************************************/
 #include<stdio.h>
 
+/*冒泡排序 交换排序*/
+void sort_1(int *s,int size)
+{
+	int i = 0;
+	int j = 0;
+	int chage = 0;
+
+	for(i = size-1; i > 0; i--)
+	{
+		chage = 0;
+		for(j = 0; j < i; j ++)
+		{
+			if (s[j] > s[j+1])
+			{
+				s[j] ^= s[j+1];
+				s[j+1] ^= s[j];
+				s[j] ^= s[j+1];
+				chage = 1;
+
+			}
+
+		}
+		if(chage == 0)
+		{
+			break;
+		}
+	}
+	return;
+}
+
+/*选择排序*.
+void sort_2(int *s,int size)
+{
+	int i = 0;
+	int j = 0;
+	int min = 0;
+
+	for(i = 0; i < size -1; i++)
+	{
+		min = i;
+		for(j=i+1;j < size; j ++)
+		{
+			if(s[j] < s[min])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			s[i] ^= s[min];
+			s[min] ^= s[i];
+			s[i] ^= s[min];
+		}
+	}
+}
+
 int main()
 {
+	int i = 0;
+	int len = 0;
+	int s[] = {14,27,9,55,16,48,32,22};
 
+	len = sizeof(s)/sizeof(int);
+    
+	sort_2(s,len);
+
+	for (i = 0; i < len; i++)
+	{
+		printf("%d,",s[i]);
+	}
 	return 0;
 }
