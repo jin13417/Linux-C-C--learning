45795d2931943c871a8ec5a9aa4dcc1cabfc8245
jinshaohui <jinshaohui789@163.com>
2018-10-19 14:46:31 +0800

    add by j00322883 for

---
+++ C/work/backtrace.c
@@ -0,0 +1,73 @@
+/*************************************************************************
+ > File Name: backtrace.c
+ > Author:  jinshaohui
+ > Mail:    jinshaohui789@163.com
+ > Time:    18-10-07
+ > Desc:    
+ ************************************************************************/
+#include<stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <execinfo.h>
+#include <signal.h>
+
+#define myassert(flg)\
+do{\
+	int pid = getpid();\
+	if(!flg)\
+	{\
+		printf("\r\n file:%s,Line:%d,fuc:%s\r\n",__FILE__,__LINE__,__FUNCTION__);\
+	    kill(pid,SIGUSR1);\
+	}\
+}while(0)
+void dump(int signo)
+{
+	void *buffer[30] = {0};
+	size_t size = 0;
+	size_t i = 0;
+	char ** strings = NULL;
+
+	size = backtrace(buffer,30);
+
+	strings = backtrace_symbols(buffer,size);
+	if(strings == NULL)
+	{
+		return;
+	}
+
+	for (i = 0 ; i< size; i++)
+	{
+		printf("%s\n",strings[i]);	
+	}
+
+	free(strings);
+
+	exit(0);
+}
+
+void trace_3()
+{
+	int * p = NULL;
+	myassert((p != NULL));
+	
+}
+
+void trace_2()
+{
+	trace_3();
+}
+
+void trace_1()
+{
+	trace_2();
+}
+
+int main()
+{
+	signal(SIGUSR1,dump);
+
+	trace_1();
+
+	return;
+}
+
